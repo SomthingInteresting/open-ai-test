5cb9b95965a9859eabe15f5209d637ef45090476
Quddus Rahman <qrahman115@gmail.com>
2023-05-25 11:49:55 +0100

    basic prompt test for open ai api

---
+++ main.py
@@ -0,0 +1,14 @@
+import openai
+import config
+
+openai.api_key = config.OPENAI_API_KEY
+response = openai.Completion.create(
+    engine="davinci",
+    prompt="Blog topics dealing with daily life living on Mars\n\n1.",
+    temperature=0.3,
+    max_tokens=64,
+    top_p=1,
+    frequency_penalty=0.5,
+    presence_penalty=0,
+)
+print(response)
