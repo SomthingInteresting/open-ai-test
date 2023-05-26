2edb10b3feb03b8f28c0c7a0e1c872d6d05cee6c
Quddus Rahman <qrahman115@gmail.com>
2023-05-26 11:15:21 +0100

    added open ai course wip file

---
+++ openAICourse.py
@@ -0,0 +1,33 @@
+import openai
+import config
+
+openai.api_key = config.OPENAI_API_KEY
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+prompt = f"""
+Provide a list of food, drinks, costumes, clothing, decorations suggestions for this party theme \ 
+use bullet points.
+```{"Gaming"}```
+"""
+response = get_completion(prompt)
+print(response)
